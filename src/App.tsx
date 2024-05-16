446aa03bb2b784b9bdac4b8f25a2903420636ba4
YellowLime77 (Matthew Yang) <matthewyanglele@gmail.com>
2024-05-15 20:10:01 -0400

    header

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,16 @@ import { Textarea } from "@/components/ui/textarea"
 
 import { ThemeProvider } from './components/theme-provider'
 
+import vocaloidle from '/Vocaloidle.svg'
+
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="w-500 h-screen bg-cyan-500 rounded-lg border shadow-lg m-8">
+      <header className="border-b flex border-cyan-600 justify-center align-middle p-2 max-h-20 bg-cyan-800 shadow">
+        <img src={vocaloidle} className="max-h-20" alt="Vocaloidle logo" />
+      </header>
+      <div className="container w-full h-screen bg-cyan-900/20 border shadow flex flex-row space-y-4 p-4 items-center justify-center">
+        <Button>Click me</Button>
       </div>
     </ThemeProvider>
   )
