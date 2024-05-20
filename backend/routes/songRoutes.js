f853953785ee2353ec68458299b982e69069f892
YellowLime77 (Matthew Yang) <matthewyanglele@gmail.com>
2024-05-19 20:54:34 -0400

    get routing

--- backend/routes/songRoutes.js
+++ backend/routes/songRoutes.js
@@ -6,15 +6,15 @@ const Song = require('../models/song');
 router.post('/upload', upload.fields([{ name: 'audio' }, { name: 'image' }]), async (req, res) => {
     console.log("uploading song...");
     try {
-    const { producer, jp, romaji, en, spotify, yt, apple } = req.body;
+    const { producer, en, jp, romaji, spotify, yt, apple } = req.body;
     const audioFileId = req.files['audio'][0].id;
     const imageFileId = req.files['image'][0].id;
 
     const newSong = new Song({
       producer,
+      en,
       jp,
       romaji,
-      en,
       spotify,
       yt,
       apple,
@@ -28,6 +28,15 @@ router.post('/upload', upload.fields([{ name: 'audio' }, { name: 'image' }]), as
   } catch (error) {
     res.status(500).send({ error: 'Error uploading files or saving song metadata' });
   }
+});
+
+router.get('/list', async (req, res) => {
+    try {
+        const songs = await Song.find();
+        res.send(songs);
+    } catch (error) {
+        res.status(500).send({ error: 'Error fetching songs' });
+    }
 });
 
 module.exports = router;
