1584605668a1047f9f4f6b4b0309dbe0dd6e46ea
YellowLime77 (Matthew Yang) <matthewyanglele@gmail.com>
2024-05-22 20:57:55 -0400

    fixed image issue

--- frontend/src/components/endingdialog.tsx
+++ frontend/src/components/endingdialog.tsx
@@ -51,13 +51,13 @@ const EndingDialog: React.FC<Props> = ({open, setOpen, won, src, imgSrc, en, jp,
 
     return (
         <div ref={endingRef} className="h-screen w-full flex fixed justify-center items-center top-0 left-0 right-0 bottom-0 bg-transparent/30 backdrop-blur-lg z-20 duration-500" onClick={onClick}>
-            <div className='container h-fit w-fit bg-slate-800 border-slate-700 border rounded-md p-8 flex flex-col space-y-2 items-center justify-center text-center shadow-lg' onClick={preventClose}>
+            <div className='container h-fit w-fit max-w-full max-h-full bg-slate-800 border-slate-700 border rounded-md p-8 flex flex-col space-y-2 items-center justify-center text-center shadow-lg' onClick={preventClose}>
                 <p className="text-4xl font-sans text-white">{won ? "Congrats!" : "Better luck next time"}</p>
-                <div className="container flex flex-col space-y-2 p-4">
-                    <div className="w-fit h-fit min-w-16 min-h-16">
-                        <img src={imgSrc} className="w-full h-full object-cover rounded-md shadow-lg" alt="Ending"/>
-                    </div>
+
+                <div className="w-fit h-fit max-w-full max-h-screen flex justify-center">
+                    <img src={imgSrc} className="annoying-image h-full w-auto aspect-square object-cover rounded-md shadow-lg" alt="Ending"/>
                 </div>
+
                 <p className='text-xl font-sans text-white'>{en + " - " + producer}</p>
                 <p className='text-lg font-sans text-white/75'>{jp}</p>
                 <p className='text-lg font-sans text-white/75'>{romaji}</p>
@@ -71,19 +71,19 @@ const EndingDialog: React.FC<Props> = ({open, setOpen, won, src, imgSrc, en, jp,
                     customAdditionalControls={[]}
                     volume={0.5}
                     preload={"auto"}
-                    className="rounded-md shadow-lg bg-cyan-50 border-2 w-full h-fit duration-300"
+                    className="rounded-md shadow-lg bg-cyan-50 border-2 w-full h-fit duration-300 hidden md:block"
                 />
 
-                <div className="container flex flex-row h-fit w-fit space-x-4 p-4">
-                    <a href={spotifyLink} target="_blank" className="container w-fit flex flex-row space-x-4 bg-green-600 text-white text-lg hover:bg-green-700 py-4 px-6 rounded-xl shadow-lg items-center text-center duration-300">
+                <div className="container flex flex-col h-fit w-fit space-y-2 p-4 md:flex-row md:space-x-2">
+                    <a href={spotifyLink} target="_blank" className="container w-fit flex flex-row space-x-4 bg-green-600 text-white text-lg hover:bg-green-700 py-2 px-3 rounded-xl shadow-lg items-center text-center duration-300">
                         <FaSpotify className="w-10 h-10"/>
                         <p>Spotify</p>
                     </a>
-                    <a href={youtubeLink} target="_blank" className="container w-fit flex flex-row space-x-4 bg-red-700 text-white text-lg hover:bg-red-800 py-4 px-6 rounded-xl shadow-lg items-center text-center duration-300">
+                    <a href={youtubeLink} target="_blank" className="container w-fit flex flex-row space-x-4 bg-red-700 text-white text-lg hover:bg-red-800 py-2 px-3 rounded-xl shadow-lg items-center text-center duration-300">
                         <FaYoutube className="w-10 h-10"/>
                         <p>YouTube</p>
                     </a>
-                    <a href={appleMusicLink} target="_blank" className="container w-fit flex flex-row space-x-4 bg-rose-600 text-white text-lg hover:bg-rose-700 py-4 px-6 rounded-xl shadow-lg items-center text-center duration-300">
+                    <a href={appleMusicLink} target="_blank" className="container w-fit flex flex-row space-x-4 bg-rose-600 text-white text-lg hover:bg-rose-700 py-2 px-3 rounded-xl shadow-lg items-center text-center duration-300">
                         <SiApplemusic className="w-10 h-10"/>
                         <p>Apple Music</p>
                     </a>
